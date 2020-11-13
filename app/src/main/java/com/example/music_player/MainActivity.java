b1315d5a9cb4b0c381c93fcb03e215116ee5d336
qsmcgogo <463803609@qq.com>
2020-11-13 09:36:52 +0800

    create my "music player" project

---
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.music_player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+author:smc
+date:2020/11/13 9:34
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
